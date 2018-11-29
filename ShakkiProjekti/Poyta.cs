9840716186970f41f165970fc29d984695da000c
carolus <caroluskotilainen@gmail.com>
2018-11-29 11:30:02 +0200

    Alku

---
+++ ShakkiProjekti/Poyta.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShakkiProjekti
+{
+    class Poyta
+    {
+        Sotilas Vsolttu1 = new Sotilas(1, 7);
+        Sotilas Vsolttu2 = new Sotilas(2, 7);
+        Sotilas Vsolttu3 = new Sotilas(3, 7);
+        Sotilas Vsolttu4 = new Sotilas(4, 7);
+        Sotilas Vsolttu5 = new Sotilas(5, 7);
+        Sotilas Vsolttu6 = new Sotilas(6, 7);
+        Sotilas Vsolttu7 = new Sotilas(7, 7);
+        Sotilas Vsolttu8 = new Sotilas(8, 7);
+    }
+}
---
+++ ShakkiProjekti/Sotilas.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace ShakkiProjekti
+{
+    class Sotilas
+    {
+        int SijaintiX;
+        int SijaintiY;
+        bool ylitys;
+        Image solttu = Image.FromFile("Resources.ShakkiHeppa.png");
+        int SallittuLiike;
+        public Sotilas(int X, int Y)
+        {
+            SijaintiX = X;
+            SijaintiY = Y;
+        }
+    }   
+}
