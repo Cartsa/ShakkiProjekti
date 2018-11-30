6b41713b4931a0db652aa49007c18a5c94e1ef47
carolus <caroluskotilainen@gmail.com>
2018-11-30 10:40:40 +0200

    Olioiden vari string

--- ShakkiProjekti/Poyta.cs
+++ ShakkiProjekti/Poyta.cs
@@ -8,23 +8,23 @@ namespace ShakkiProjekti
 {
     class Poyta
     {
-        Sotilas vSolttu1 = new Sotilas(1, 7);
-        Sotilas vSolttu2 = new Sotilas(2, 7);
-        Sotilas vSolttu3 = new Sotilas(3, 7);
-        Sotilas vSolttu4 = new Sotilas(4, 7);
-        Sotilas vSolttu5 = new Sotilas(5, 7);
-        Sotilas vSolttu6 = new Sotilas(6, 7);
-        Sotilas vSolttu7 = new Sotilas(7, 7);
-        Sotilas vSolttu8 = new Sotilas(8, 7);
+        Sotilas vSolttu1 = new Sotilas(1, 7, "Valkoinen");
+        Sotilas vSolttu2 = new Sotilas(2, 7, "Valkoinen");
+        Sotilas vSolttu3 = new Sotilas(3, 7, "Valkoinen");
+        Sotilas vSolttu4 = new Sotilas(4, 7, "Valkoinen");
+        Sotilas vSolttu5 = new Sotilas(5, 7, "Valkoinen");
+        Sotilas vSolttu6 = new Sotilas(6, 7, "Valkoinen");
+        Sotilas vSolttu7 = new Sotilas(7, 7, "Valkoinen");
+        Sotilas vSolttu8 = new Sotilas(8, 7, "Valkoinen");
 
-        Sotilas mSolttu1 = new Sotilas(1, 2);
-        Sotilas mSolttu2 = new Sotilas(2, 2);
-        Sotilas mSolttu3 = new Sotilas(3, 2);
-        Sotilas mSolttu4 = new Sotilas(4, 2);
-        Sotilas mSolttu5 = new Sotilas(5, 2);
-        Sotilas mSolttu6 = new Sotilas(6, 2);
-        Sotilas mSolttu7 = new Sotilas(7, 2);
-        Sotilas mSolttu8 = new Sotilas(8, 2);
+        Sotilas mSolttu1 = new Sotilas(1, 2, "Musta");
+        Sotilas mSolttu2 = new Sotilas(2, 2, "Musta");
+        Sotilas mSolttu3 = new Sotilas(3, 2, "Musta");
+        Sotilas mSolttu4 = new Sotilas(4, 2, "Musta");
+        Sotilas mSolttu5 = new Sotilas(5, 2, "Musta");
+        Sotilas mSolttu6 = new Sotilas(6, 2, "Musta");
+        Sotilas mSolttu7 = new Sotilas(7, 2, "Musta");
+        Sotilas mSolttu8 = new Sotilas(8, 2, "Musta");
 
 
     }
