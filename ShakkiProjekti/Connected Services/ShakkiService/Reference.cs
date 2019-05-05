f3919ef5178de2c766b8f647343dd260fda28dac
carolus <caroluskotilainen@gmail.com>
2019-05-05 19:31:44 +0300

    Lopetus.cs ja service toimii

--- ShakkiProjekti/Connected Services/ShakkiService/Reference.cs
+++ ShakkiProjekti/Connected Services/ShakkiService/Reference.cs
@@ -9,86 +9,17 @@
 //------------------------------------------------------------------------------
 
 namespace ShakkiProjekti.ShakkiService {
-    using System.Runtime.Serialization;
-    using System;
     
-    
-    [System.Diagnostics.DebuggerStepThroughAttribute()]
-    [System.CodeDom.Compiler.GeneratedCodeAttribute("System.Runtime.Serialization", "4.0.0.0")]
-    [System.Runtime.Serialization.DataContractAttribute(Name="CompositeType", Namespace="http://schemas.datacontract.org/2004/07/ShakkiProjektinWCF")]
-    [System.SerializableAttribute()]
-    public partial class CompositeType : object, System.Runtime.Serialization.IExtensibleDataObject, System.ComponentModel.INotifyPropertyChanged {
-        
-        [System.NonSerializedAttribute()]
-        private System.Runtime.Serialization.ExtensionDataObject extensionDataField;
-        
-        [System.Runtime.Serialization.OptionalFieldAttribute()]
-        private bool BoolValueField;
-        
-        [System.Runtime.Serialization.OptionalFieldAttribute()]
-        private string StringValueField;
-        
-        [global::System.ComponentModel.BrowsableAttribute(false)]
-        public System.Runtime.Serialization.ExtensionDataObject ExtensionData {
-            get {
-                return this.extensionDataField;
-            }
-            set {
-                this.extensionDataField = value;
-            }
-        }
-        
-        [System.Runtime.Serialization.DataMemberAttribute()]
-        public bool BoolValue {
-            get {
-                return this.BoolValueField;
-            }
-            set {
-                if ((this.BoolValueField.Equals(value) != true)) {
-                    this.BoolValueField = value;
-                    this.RaisePropertyChanged("BoolValue");
-                }
-            }
-        }
-        
-        [System.Runtime.Serialization.DataMemberAttribute()]
-        public string StringValue {
-            get {
-                return this.StringValueField;
-            }
-            set {
-                if ((object.ReferenceEquals(this.StringValueField, value) != true)) {
-                    this.StringValueField = value;
-                    this.RaisePropertyChanged("StringValue");
-                }
-            }
-        }
-        
-        public event System.ComponentModel.PropertyChangedEventHandler PropertyChanged;
-        
-        protected void RaisePropertyChanged(string propertyName) {
-            System.ComponentModel.PropertyChangedEventHandler propertyChanged = this.PropertyChanged;
-            if ((propertyChanged != null)) {
-                propertyChanged(this, new System.ComponentModel.PropertyChangedEventArgs(propertyName));
-            }
-        }
-    }
     
     [System.CodeDom.Compiler.GeneratedCodeAttribute("System.ServiceModel", "4.0.0.0")]
     [System.ServiceModel.ServiceContractAttribute(ConfigurationName="ShakkiService.IService1")]
     public interface IService1 {
         
-        [System.ServiceModel.OperationContractAttribute(Action="http://tempuri.org/IService1/GetData", ReplyAction="http://tempuri.org/IService1/GetDataResponse")]
-        string GetData(int value);
+        [System.ServiceModel.OperationContractAttribute(Action="http://tempuri.org/IService1/Tallennus", ReplyAction="http://tempuri.org/IService1/TallennusResponse")]
+        string Tallennus(string ValkoinenNimimerkki, string MustaNimimerkki, int Vuorot, string Voittaja);
         
-        [System.ServiceModel.OperationContractAttribute(Action="http://tempuri.org/IService1/GetData", ReplyAction="http://tempuri.org/IService1/GetDataResponse")]
-        System.Threading.Tasks.Task<string> GetDataAsync(int value);
-        
-        [System.ServiceModel.OperationContractAttribute(Action="http://tempuri.org/IService1/GetDataUsingDataContract", ReplyAction="http://tempuri.org/IService1/GetDataUsingDataContractResponse")]
-        ShakkiProjekti.ShakkiService.CompositeType GetDataUsingDataContract(ShakkiProjekti.ShakkiService.CompositeType composite);
-        
-        [System.ServiceModel.OperationContractAttribute(Action="http://tempuri.org/IService1/GetDataUsingDataContract", ReplyAction="http://tempuri.org/IService1/GetDataUsingDataContractResponse")]
-        System.Threading.Tasks.Task<ShakkiProjekti.ShakkiService.CompositeType> GetDataUsingDataContractAsync(ShakkiProjekti.ShakkiService.CompositeType composite);
+        [System.ServiceModel.OperationContractAttribute(Action="http://tempuri.org/IService1/Tallennus", ReplyAction="http://tempuri.org/IService1/TallennusResponse")]
+        System.Threading.Tasks.Task<string> TallennusAsync(string ValkoinenNimimerkki, string MustaNimimerkki, int Vuorot, string Voittaja);
     }
     
     [System.CodeDom.Compiler.GeneratedCodeAttribute("System.ServiceModel", "4.0.0.0")]
@@ -118,20 +49,12 @@ namespace ShakkiProjekti.ShakkiService {
                 base(binding, remoteAddress) {
         }
         
-        public string GetData(int value) {
-            return base.Channel.GetData(value);
+        public string Tallennus(string ValkoinenNimimerkki, string MustaNimimerkki, int Vuorot, string Voittaja) {
+            return base.Channel.Tallennus(ValkoinenNimimerkki, MustaNimimerkki, Vuorot, Voittaja);
         }
         
-        public System.Threading.Tasks.Task<string> GetDataAsync(int value) {
-            return base.Channel.GetDataAsync(value);
-        }
-        
-        public ShakkiProjekti.ShakkiService.CompositeType GetDataUsingDataContract(ShakkiProjekti.ShakkiService.CompositeType composite) {
-            return base.Channel.GetDataUsingDataContract(composite);
-        }
-        
-        public System.Threading.Tasks.Task<ShakkiProjekti.ShakkiService.CompositeType> GetDataUsingDataContractAsync(ShakkiProjekti.ShakkiService.CompositeType composite) {
-            return base.Channel.GetDataUsingDataContractAsync(composite);
+        public System.Threading.Tasks.Task<string> TallennusAsync(string ValkoinenNimimerkki, string MustaNimimerkki, int Vuorot, string Voittaja) {
+            return base.Channel.TallennusAsync(ValkoinenNimimerkki, MustaNimimerkki, Vuorot, Voittaja);
         }
     }
 }
